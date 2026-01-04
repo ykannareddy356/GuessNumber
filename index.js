a32967c23b86f1ae2c6821fccabb8ceb971ac9a6
Yerram Kanna Venkateswara Reddy <ykannareddy356@gmail.com>
2026-01-04 12:54:28 +0530

    addcomm

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+//randomnumber is the number to be guessedgit
 let randomNumber = Math.floor(Math.random() * 100) + 1;
 const guesses = document.querySelector(".guesses");
 const lastResult = document.querySelector(".lastResult");
